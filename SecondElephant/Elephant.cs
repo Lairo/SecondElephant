788f89ed670002733f249d3e2073d27733640ff9
chajus1 <chajus1@googlemail.com>
2023-05-29 23:07:58 +0100

    Add project files.

---
+++ SecondElephant/Elephant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondElephant
+{
+    internal class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {Name}.\nMy ears are {EarSize} inches tall\n");
+        }
+    }
+}
---
+++ SecondElephant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace SecondElephant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(5);
+        }
+    }
+}
